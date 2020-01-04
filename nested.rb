5e32b1186b09aa84e5aea0c6e03d8807bee6f84c
Aaron Minyard <amnyrrd@gmail.com>
2020-01-04 00:10:13 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -118,6 +118,6 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
-    programmer_hash[:dennis_ritchie][:languages].merge("Assembly")
+    programmer_hash[:dennis_ritchie][:languages].push("Assembly")
  programmer_hash 
 end
